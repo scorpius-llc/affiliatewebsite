ccde507e9e41e3e1d66e11792d17ddab84da6085
redhatskrad <113696107+redhatskrad@users.noreply.github.com>
2026-06-03 19:29:14 -0400

    fix last commit

---
+++ app/reviews/layout.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  metadataBase: new URL('https://www.the-pool-lab.com'),
+  alternates: {
+    canonical: 'https://www.the-pool-lab.com/reviews',
+  },
+};
+
+export default function ReviewsLayout({ children }) {
+  return children;
+}
